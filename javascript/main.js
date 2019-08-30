cc31e1948586f33430876d65b3761cbb2193550e
codemouse <freedmouse@gmail.com>
2019-08-30 16:42:07 -0700

    updated meta tags and projects

---
+++ javascript/main.js
@@ -0,0 +1,63 @@
+///////////////////////////////////////////////////////////////
+////////////                                    ////////////////
+///////////         VARIABLE ASSIGNMENTS        ///////////////
+///////////                                     //////////////
+//////////////////////////////////////////////////////////////
+var meta = `<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="title" content="Christian Web Developer">
+<meta name="description"
+  content="The website of William H. Hall III, christian web developer. Offering web design and development services at very reasonable and affordable prices.">
+<meta name="keywords"
+  content="web design, web development, christian, tucson, az, SEO, christian web developers, application development, mobile friendly, cutting edge design, consulting services, database management, web, websites, full stack developer">
+<meta name="robots" content="index, follow">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="language" content="English">
+<meta name="author" content="William H. Hall III">`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
+////////////                                    ////////////////
+///////////             FUNCTIONS               ///////////////
+///////////                                     //////////////
+//////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////
+////////////                                    ////////////////
+///////////             PROCESSES              ///////////////
+///////////                                     //////////////
+//////////////////////////////////////////////////////////////
+
+$(".navbar-brand").text("William H. Hall III - Christian Web Developer");
+$(".font-weight-light").addClass("business-name");
+// $("head").html(meta);
+// $(".business-name").text("Christian Web Developer");
+// $(".text-white").addClass("action-call");
+// $(".action-call").text("Churches, get your FREE* website now! Limited time only!");
+// $(".navbar-nav").append(`<li class="nav-item">
+// <a class="nav-link" href="services.html">Testimony</a>
+// </li>`)
+// $(".image-fluid").html(`<img src="assets/images/collect_crystals.png">`);
+
