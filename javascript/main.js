1384363c571dc0c9c97f892f741f14bf9a31e7da
codemouse <freedmouse@gmail.com>
2019-09-12 15:28:10 -0700

    created and added logo to navbar

--- javascript/main.js
+++ javascript/main.js
@@ -61,8 +61,7 @@ $( ".questions" ).click(function() {
 ///////////                                     //////////////
 //////////////////////////////////////////////////////////////
 
-$(".navbar-brand").text("William H. Hall III - Full Stack Web Developer").css("color", "#2CB31E");
-// $(".navbar-brand").css("color", "#2CB31E");
+$(".navbar-brand").text("William H. Hall III - Front End Web Developer").css("color", "#2CB31E");
 $(".font-weight-light").addClass("business-name");
 $(".nav-link").css("color", "#FF7A24");
 $(".current").css("color","#FFF");
